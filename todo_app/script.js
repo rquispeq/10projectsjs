854582c2d5937ffa1fa1e82cee4f9d9a696a4a5c
Rony Quispe <oghmand@gmail.com>
2025-01-24 09:46:44 -0500

    add element to do to the interface

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,23 +1,30 @@
 const form_todo = document.getElementById('form_todo')
 const todo_list = document.getElementById('todo_list')
+const txt_todo = document.getElementById('txt_todo')
 const data = []
 form_todo.addEventListener('submit', e => {
     e.preventDefault()
     const formData =  new FormData(e.target)
     formData.forEach(val => {
         data.push(val)
-        console.log(data)
+        txt_todo.value = ""
     })
 
     fillList()
 })
 
 function fillList(){
-    data.forEach(val => {
+    // data.forEach(val => {
+        const val = data[data.length-1]
         let newDiv = document.createElement('div')
-        newDiv.classList.add('txt_todo')
+        newDiv.classList.add('task')
         let content = document.createTextNode(val)
         newDiv.appendChild(content)
         todo_list.appendChild(newDiv)
-    })
+    // })
 }
+
+
+// complete task
+
+const tasks = document.getElementsByTagName("task")
