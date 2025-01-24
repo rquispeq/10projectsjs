471bbbf75f48f2b5db8473f31c8851231aa38eca
Rony Quispe <oghmand@gmail.com>
2025-01-24 10:05:05 -0500

    feature done an undone task and remove task

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,30 +1,42 @@
-const form_todo = document.getElementById('form_todo')
-const todo_list = document.getElementById('todo_list')
-const txt_todo = document.getElementById('txt_todo')
-const data = []
-form_todo.addEventListener('submit', e => {
-    e.preventDefault()
-    const formData =  new FormData(e.target)
-    formData.forEach(val => {
-        data.push(val)
-        txt_todo.value = ""
-    })
+const form_todo = document.getElementById("form_todo");
+const todo_list = document.getElementById("todo_list");
+const txt_todo = document.getElementById("txt_todo");
+const data = [];
+form_todo.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    formData.forEach((val) => {
+        data.push(val);
+        txt_todo.value = "";
+    });
 
-    fillList()
-})
+    fillList();
+});
 
-function fillList(){
+function fillList() {
     // data.forEach(val => {
-        const val = data[data.length-1]
-        let newDiv = document.createElement('div')
-        newDiv.classList.add('task')
-        let content = document.createTextNode(val)
-        newDiv.appendChild(content)
-        todo_list.appendChild(newDiv)
+    const val = data[data.length - 1];
+    let newTask = document.createElement("div");
+    newTask.classList.add("task");
+    let content = document.createTextNode(val);
+    newTask.appendChild(content);
+    todo_list.appendChild(newTask);
     // })
+    featureToggleTask(newTask);
+    functionRemoveTask(newTask);
 }
-
 
 // complete task
 
-const tasks = document.getElementsByTagName("task")
+const featureToggleTask = task => {
+    task.addEventListener("click", (e) => {
+        task.classList.toggle("done");
+    });
+};
+
+const functionRemoveTask = task =>{
+    task.addEventListener("contextmenu", (e) => {
+        e.preventDefault()
+        task.remove()
+    });
+}
