52cd4f52a36f7931ac0bdc4e955093febd0bc46b
Rony Quispe <oghmand@gmail.com>
2025-02-05 10:44:48 -0500

    change some styles for image background

--- weather_app/src/components/WeatherInfo.js
+++ weather_app/src/components/WeatherInfo.js
@@ -1,6 +1,10 @@
 export default function WeatherInfo( {weather} ) {
   return (
-    <div className="main-info">
+    <div className="main-info" style={
+      {
+        backgroundImage: `url(https://www.thecityofldn.com/wp-content/uploads/2023/05/jamiesmithphoto-2162-Tower-Bridge-with-City-behind-reduced-2000x1334.jpg)`
+      }
+    }>
       <div className="city">{weather.request.query}</div>
       <div className="current-weather">
         <div className="tempeture">{weather.current.temperature}°</div>
